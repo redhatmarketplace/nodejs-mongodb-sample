da9b0e398465479a37d76baeda151c45903b4170
rojanjose <rojanjose@gmail.com>
2020-06-30 16:50:57 -0400

    mongo with ssl connection

--- routes/messages.js
+++ routes/messages.js
@@ -4,8 +4,31 @@
  *--------------------------------------------------------------------------------------------*/
 
 const mongoose = require('mongoose')
+const fs = require('fs')
 
-const mongoURI = "mongodb://user:password@mongodb:27017/guestbook"
+//const mongoURI = "mongodb://user:password@mongodb:27017/guestbook"
+//const mongoURI = "mongodb://localhost:27017/guestbook"
+//Tested with mongo running locally.
+//const mongoURI = "mongodb://localhost:27017/user1-guestbook"
+
+const username = "ibm_cloud_deb97559_5228_4f90_89bb_baa9c1ac82be"
+const password = "93b76eecc9aa70da306d18129c317d6a78f3d27192eb0bd5ca0abb8cd542eac5"
+const server = "f25f8cd3-dafc-4cc5-9764-5894e80681b4-0.brjdfmfw09op3teml03g.databases.appdomain.cloud:30184,f25f8cd3-dafc-4cc5-9764-5894e80681b4-1.brjdfmfw09op3teml03g.databases.appdomain.cloud:30184"
+const database = "guestbook"
+const mongoURI = "mongodb://" +
+                username + ":" + password + "@" + server + "/" + database +
+                "?authSource=admin&replicaSet=replset"
+
+var sslCA = [fs.readFileSync('./keys/mongodb.cert')];
+var options = {
+  useNewUrlParser: true,
+  connectTimeoutMS: 20000,
+  reconnectTries: 1,
+  ssl: true,
+  sslValidate: true,
+  sslCA,
+};
+
 
 const db = mongoose.connection;
 
@@ -25,7 +48,10 @@ const connectToMongoDB = async () => {
     await mongoose.connect(mongoURI, {
         useNewUrlParser: true,
         connectTimeoutMS: 2000,
-        reconnectTries: 1
+        reconnectTries: 1,
+        ssl: true,
+        sslValidate: true,
+        sslCA,
     })
 };
 
